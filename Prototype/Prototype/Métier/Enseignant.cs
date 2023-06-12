9d4344e9736ae44298103bdbf7a897df68bb38d2
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2023-06-12 10:55:41 +0200

    Classe ApplicationData et Enseignant

--- Prototype/Prototype/Métier/Enseignant.cs
+++ Prototype/Prototype/Métier/Enseignant.cs
@@ -1,101 +1,42 @@
 using System;
+using System.Collections.ObjectModel;
 
 public class Enseignant : CRUD
 {
-   public int getidPersonnel()
-   {
-      return idPersonnel;
-   }
-   
-   public void setidPersonnel(int newIdPersonnel)
-   {
-      this.idPersonnel = newIdPersonnel;
-   }
-   
-   public String getmail()
-   {
-      return mail;
-   }
-   
-   public void setmail(String newMail)
-   {
-      this.mail = newMail;
-   }
-   
-   public String getnomPersonnel()
-   {
-      return nomPersonnel;
-   }
-   
-   public void setnomPersonnel(String newNomPersonnel)
-   {
-      this.nomPersonnel = newNomPersonnel;
-   }
-   
-   public String getprenomPersonnel()
-   {
-      return prenomPersonnel;
-   }
-   
-   public void setprenomPersonnel(String newPrenomPersonnel)
-   {
-      this.prenomPersonnel = newPrenomPersonnel;
-   }
-   
-   public Enseignant()
-   {
-      // TODO: implement
-   }
+    public int IdPersonnel { get; set; }
+    public string Email { get; set; }
+    public string NomPersonnel { get; set; }
+    public string PrenomPersonnel { get; set; }
+    public ObservableCollection<Attribution> LesAttributions;
 
-   public System.Collections.ArrayList attribution;
-   
-   /// <pdGenerated>default getter</pdGenerated>
-   public System.Collections.ArrayList GetAttribution()
-   {
-      if (attribution == null)
-         attribution = new System.Collections.ArrayList();
-      return attribution;
-   }
-   
-   /// <pdGenerated>default setter</pdGenerated>
-   public void SetAttribution(System.Collections.ArrayList newAttribution)
-   {
-      RemoveAllAttribution();
-      foreach (Attribution oAttribution in newAttribution)
-         AddAttribution(oAttribution);
-   }
-   
-   /// <pdGenerated>default Add</pdGenerated>
-   public void AddAttribution(Attribution newAttribution)
-   {
-      if (newAttribution == null)
-         return;
-      if (this.attribution == null)
-         this.attribution = new System.Collections.ArrayList();
-      if (!this.attribution.Contains(newAttribution))
-         this.attribution.Add(newAttribution);
-   }
-   
-   /// <pdGenerated>default Remove</pdGenerated>
-   public void RemoveAttribution(Attribution oldAttribution)
-   {
-      if (oldAttribution == null)
-         return;
-      if (this.attribution != null)
-         if (this.attribution.Contains(oldAttribution))
-            this.attribution.Remove(oldAttribution);
-   }
-   
-   /// <pdGenerated>default removeAll</pdGenerated>
-   public void RemoveAllAttribution()
-   {
-      if (attribution != null)
-         attribution.Clear();
-   }
+    public Enseignant()
+    {
+    }
+    public Enseignant(int idPersonnel, string email, string nomPersonnel, string prenomPersonnel)
+    {
+        this.IdPersonnel = idPersonnel;
+        this.Email = email;
+        this.NomPersonnel = nomPersonnel;
+        this.PrenomPersonnel = prenomPersonnel;
+    }
 
-   private int idPersonnel;
-   private String mail;
-   private String nomPersonnel;
-   private String prenomPersonnel;
+    public override void Create()
+    {
 
+    }
+
+    public override void Remove()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Delete()
+    {
+
+    }
 }

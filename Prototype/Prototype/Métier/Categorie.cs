34513808f8aae4209dad071dbe9bc73c64cc61ec
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2023-06-12 11:30:09 +0200

    Categorie : FindAll()

--- Prototype/Prototype/Métier/Categorie.cs
+++ Prototype/Prototype/Métier/Categorie.cs
@@ -1,6 +1,7 @@
 using Prototype.Métier;
 using System;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Runtime.CompilerServices;
 
 namespace Prototype.Métier
@@ -41,7 +42,21 @@ namespace Prototype.Métier
 
         public ObservableCollection<Categorie> FindAll()
         {
-            throw new NotImplementedException();
+            ObservableCollection<Categorie> lesCategories = new ObservableCollection<Categorie>();
+
+            DataAccess accesBD = new DataAccess();
+            String requete = "SELECT * FROM Categorie";
+            DataTable datas = accesBD.GetData(requete);
+
+            if (datas != null)
+            {
+                foreach (DataRow row in datas.Rows)
+                {
+                    Categorie c = new Categorie(int.Parse(row["idCategorie"].ToString()), (String)row["nomCategorie"]);
+                    lesCategories.Add(c);
+                }
+            }
+            return lesCategories;
         }
     }
 }

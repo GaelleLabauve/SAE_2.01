5fe28d188630b188509e8380819324b43534a2d1
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2023-06-13 17:35:11 +0200

    Add() et Remove() dans ApplicationData

--- Prototype/Prototype/Metier/ApplicationData.cs
+++ Prototype/Prototype/Metier/ApplicationData.cs
@@ -56,23 +56,42 @@ namespace Prototype.Metier
 
         }
 
-        public void Remove(Categorie c)
+        public void Add(object obj)
         {
-            this.LesCategories.Remove(c);
-        }
-        public void Remove(Attribution c)
-        {
-            this.LesAttributions.Remove(c);
-        }
-        public void Remove(Enseignant e)
-        {
-            this.LesEnseignants.Remove(e);
+            if (obj is Categorie)
+            {
+                this.LesCategories.Add((Categorie)obj);
+            }
+            else if (obj is Attribution)
+            {
+                this.LesAttributions.Add((Attribution)obj);
+            }
+            else if (obj is Enseignant)
+            {
+                this.LesEnseignants.Add((Enseignant)obj);
+            }
+            else if (obj is Materiel)
+            {
+                this.LesMateriels.Add((Materiel)obj);
+            }
         }
         public void Remove(object obj)
         {
             if (obj is Categorie)
             {
-
+                this.LesCategories.Remove((Categorie)obj);
+            }
+            else if (obj is Attribution)
+            {
+                this.LesAttributions.Remove((Attribution)obj);
+            }
+            else if (obj is Enseignant)
+            {
+                this.LesEnseignants.Remove((Enseignant)obj);
+            }
+            else if (obj is Materiel)
+            {
+                this.LesMateriels.Remove((Materiel)obj);
             }
         }
     }

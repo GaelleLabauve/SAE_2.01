52260577721da09a89d709175e7a312f404cd30d
GaelleLabauve <gaelle.labauve@etu.univ-smb.fr>
2023-06-16 15:03:09 +0200

    Création test de categorie + ajout d'une constante pour la limite de caractere

--- Prototype/Prototype/Metier/Categorie.cs
+++ Prototype/Prototype/Metier/Categorie.cs
@@ -14,6 +14,7 @@ namespace Prototype.Metier
     /// </summary>
     public class Categorie : Crud<Categorie>
     {
+        public const int LIMITE_TAILLE_CARACTERE_NOM = 50;
         /// <summary>
         /// Obtient ou definit l'idCategorie de cette catégorie –
         /// </summary>

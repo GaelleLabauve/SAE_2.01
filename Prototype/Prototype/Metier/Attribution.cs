cf996fdcce625b9832218f9b0cdc7155db2fe53c
GaelleLabauve <gaelle.labauve@etu.univ-smb.fr>
2023-06-16 14:21:27 +0200

    Mise en place commentaire de Attribution

--- Prototype/Prototype/Metier/Attribution.cs
+++ Prototype/Prototype/Metier/Attribution.cs
@@ -6,16 +6,52 @@ namespace Prototype.Metier
 {
     public class Attribution : Crud<Attribution>
     {
+        /// <summary>
+        /// Obtient ou definit l'idMateriel de cette attribution. Permet de trouver le materiel.
+        /// </summary>
         public int FK_IdMateriel { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit l'idPersonnel de cette attribution. Permet de trouver l'enseignant.
+        /// </summary>
         public int FK_IdPersonnel { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit la date de cette attribution.
+        /// </summary>
         public DateTime DateAttribution { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit le commentaire de cette attribution.
+        /// </summary>
         public string CommentaireAttribution { get; set; }
+
+
+        /// <summary>
+        /// Obtient ou definit l'enseignant de cette attribution.
+        /// </summary>
         public Enseignant UnEnseignant { get; set; }
+
+
+        /// <summary>
+        /// Obtient ou definit le materiel de cette attribution.
+        /// </summary>
         public Materiel UnMateriel { get; set; }
 
+        /// <summary>
+        /// Cree une attribution vide.
+        /// </summary>
         public Attribution()
         {
         }
+
+        /// <summary>
+        /// Cree une attribution.
+        /// </summary>
+        /// <param name="fK_IdMateriel"> un entier donnant la cle unique du materiel de cette attribution</param>
+        /// <param name="fK_IdPersonnel"> un entier donnant la cle unique de l'enseignant(e) de cette attribution</param>
+        /// <param name="dateAttribution"> un dateTime donnant la date de cette attribution</param>
+        /// <param name="commentaireAttribution"> une chaine de caractere donnant le commentaire de cette attribution</param>
         public Attribution(int fK_IdMateriel, int fK_IdPersonnel, DateTime dateAttribution, string commentaireAttribution)
         {
             this.FK_IdMateriel = fK_IdMateriel;
@@ -24,6 +60,9 @@ namespace Prototype.Metier
             this.CommentaireAttribution = commentaireAttribution;
         }
 
+        /// <summary>
+        /// Cree dans la base de donnee cette attribution.
+        /// </summary>
         public void Create()
         {
             DataAccess accesBD = new DataAccess();
@@ -35,18 +74,28 @@ namespace Prototype.Metier
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Mets a jour dans la base de donnee cette attribution.
+        /// </summary>
         public void Update()
         {
             DataAccess accesBD = new DataAccess();
             DataTable datas = accesBD.GetData($"UPDATE EST_ATTRIBUE SET commentaireAttribution='{this.CommentaireAttribution}' WHERE idMateriel='{this.FK_IdMateriel}' AND idPersonnel='{this.FK_IdPersonnel}' AND dateAttribution='{this.DateAttribution}';");
         }
 
+        /// <summary>
+        /// Supprime de la base de donnee cette attribution.
+        /// </summary>
         public void Delete()
         {
             DataAccess accesBD = new DataAccess();
             DataTable datas = accesBD.GetData($"DELETE FROM EST_ATTRIBUE WHERE idMateriel={this.FK_IdMateriel} AND idPersonnel='{this.FK_IdPersonnel}';");
         }
 
+        /// <summary>
+        /// Parcours la table EST_ATTRIBUE de la base de donnee.
+        /// </summary>
+        /// <returns> ObservableCollection<Attribution> regroupant toutes les attributions mise dans la base de donnee</returns>
         public ObservableCollection<Attribution> FindAll()
         {
             ObservableCollection<Attribution> lesAttributions = new ObservableCollection<Attribution>();
